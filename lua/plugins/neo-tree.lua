f1f68b53b0ec46d4d535024c7b9d64dd8a94bc81
Le Tan <tamlokveer@gmail.com>
2025-11-10 14:29:48 +0800

    NeoTree: enable copying words in the pane

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,9 +2,22 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    enable_normal_mode_for_inputs = true,
     window = {
       mappings = {
         ["<space>"] = "none",
+        ["h"] = "none",
+        ["j"] = "none",
+        ["k"] = "none",
+        ["l"] = "none",
+        ["w"] = "none",
+        ["e"] = "none",
+        ["b"] = "none",
+        ["W"] = "none",
+        ["E"] = "none",
+        ["B"] = "none",
+        ["y"] = "none",
+        ["Y"] = "none",
       },
     },
   },
