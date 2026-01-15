900e4f20cca79ff8a5556113324d760bfb62c2bd
Le Tan <tamlokveer@gmail.com>
2026-01-15 13:49:30 +0800

    refine neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -8,6 +8,19 @@ return {
   },
   cmd = "Neogit",
   keys = {
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+    { "<leader>gf",
+      function() require('neogit').open({ kind = "floating" }) end,
+      desc = "Show Neogit UI float"
+    }
+  },
+  opts = {
+    auto_refresh = false,
+    sections = {
+      recent = {
+        folded = true,
+        hidden = true,
+      }
+    }
   }
 }
