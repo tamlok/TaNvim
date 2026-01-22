8dbe55ad22d35d248b9e99cc52dbb5177c23a8cd
Le Tan <tamlokveer@gmail.com>
2026-01-22 22:40:28 +0800

    supress same notification in short time

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,19 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+local original_notify = vim.notify
+local last = {}
+
+vim.notify = function(msg, level, opts)
+  local key = tostring(level or "info") .. ":" .. msg
+  local now = vim.loop.now()
+  local window_ms = 2000
+
+  if last[key] and (now - last[key]) < window_ms then
+    return
+  end
+
+  last[key] = now
+  return original_notify(msg, level, opts)
+end
