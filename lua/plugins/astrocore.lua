074c2b6a8b8958a9db5c0f6613c152c48b300d36
Le Tan <tamlokveer@gmail.com>
2026-04-15 23:41:00 +0800

    feat: migrate AstroNvim v6 core config
    Port the old core editor, LSP, and UI defaults into the new v6 entrypoints so the template behaves like the previous setup while staying compatible with AstroNvim v6.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -10,75 +8,126 @@ return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
   opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- passed to `vim.filetype.add`
-    filetypes = {
-      -- see `:h vim.filetype.add` for usage
-      extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
-      },
-      pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
-      },
-    },
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        wrap = true,
+        guifont = { "SauceCodePro Nerd Font Mono:h13" },
+        clipboard = {},
+        laststatus = 2,
+        scrolloff = 3,
+        colorcolumn = "101",
+        list = true,
+        listchars = { tab = "▸ ", trail = "●", extends = "❯", precedes = "❮", nbsp = "␣" },
+        fileformat = "unix",
+        fileformats = { "unix", "dos", "mac" },
       },
       g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
       },
     },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
+        ["<Leader>r"] = { desc = "Run" },
+        ["<Leader>ra"] = {
           function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
+            if vim.fn.exists(":AsyncRun") > 0 then
+              local keys = vim.api.nvim_replace_termcodes(":AsyncRun ", true, false, true)
+              vim.api.nvim_feedkeys(keys, "n", false)
+            end
           end,
-          desc = "Close buffer from tabline",
+          desc = "Run an async command",
         },
-
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<Leader>w"] = { desc = "Windows" },
+        ["<Leader>ww"] = { "<cmd>w<cr>", desc = "Save" },
+        ["<Leader>wq"] = { "<cmd>confirm q<cr>", desc = "Quit" },
+        ["<Leader>wQ"] = { "<cmd>confirm qall<cr>", desc = "Quit all" },
+        ["<Leader>wt"] = { "<cmd>tabedit<cr>", desc = "New tab" },
+        ["<Leader>wT"] = { "<cmd>tabclose<cr>", desc = "Close tab" },
+        ["<Leader>wz"] = {
+          function()
+            if vim.t.zoomed == true then
+              vim.cmd("execute t:zoom_winresetcmd")
+              vim.t.zoomed = false
+            else
+              vim.t.zoom_winresetcmd = vim.fn.winrestcmd()
+              vim.cmd "resize"
+              vim.cmd "vertical resize"
+              vim.t.zoomed = true
+            end
+          end,
+          desc = "Zoom/Restore current window",
+        },
+        ["<Leader>wc"] = {
+          function()
+            local before_cnt = vim.fn.winnr "$"
+            vim.cmd "copen"
+            if before_cnt == vim.fn.winnr "$" then vim.cmd "cclose" end
+          end,
+          desc = "Toggle quickfix list",
+        },
+        ["<Leader>wl"] = {
+          function()
+            local before_cnt = vim.fn.winnr "$"
+            vim.cmd "lopen"
+            if before_cnt == vim.fn.winnr "$" then vim.cmd "lclose" end
+          end,
+          desc = "Toggle location list",
+        },
+        ["<Leader>we"] = { "<cmd>Neotree toggle reveal<cr>", desc = "Toggle explorer" },
+        ["<Leader>wo"] = {
+          function()
+            if vim.bo.filetype == "neo-tree" then
+              vim.cmd.wincmd "p"
+            else
+              vim.cmd.Neotree "focus"
+            end
+          end,
+          desc = "Toggle explorer focus",
+        },
+        ["]o"] = { "<cmd>tnext<cr>", desc = "Next tag" },
+        ["[o"] = { "<cmd>tprevious<cr>", desc = "Previous tag" },
+        ["]O"] = { "<cmd>tlast<cr>", desc = "Last tag" },
+        ["[O"] = { "<cmd>tfirst<cr>", desc = "First tag" },
+        ["<Leader>="] = { '"+', desc = "Use the selection register" },
+        ["<Leader>-"] = { '"_', desc = "Use the black hole register" },
+        ["<Leader>."] = { '<cmd>"+yiw<cr>', desc = "Yank the word under cursor" },
+        ["<Leader><Space>"] = { "<cmd>nohlsearch<cr>", desc = "No highlight search" },
+        ["<Leader>fd"] = {
+          function()
+            local dir = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":p:h")
+            require("snacks").picker.files { cwd = dir }
+          end,
+          desc = "Find files in current buffer's directory",
+        },
+        ["<Leader>fa"] = {
+          function() require("ctags-outline").snacks_ctags_outline() end,
+          desc = "Find CTags outlines in current buffer",
+        },
+        ["<Leader>fA"] = {
+          function() require("ctags-outline").snacks_ctags_outline { buf = "all" } end,
+          desc = "Find CTags outlines in all open buffers",
+        },
+        ["<Leader>fW"] = {
+          function() require("snacks").picker.grep { hidden = true, ignored = false } end,
+          desc = "Find words in all files (including hidden, excluding ignored)",
+        },
+        ["<Leader>a"] = { desc = "AI" },
+        ["<Leader>ar"] = {
+          function()
+            require("opencode").stop()
+            require("opencode").start()
+          end,
+          desc = "Restart opencode server",
+        },
+        ["j"] = false,
+        ["k"] = false,
+        ["<Leader>e"] = false,
+        ["<Leader>o"] = false,
+      },
+      v = {
+        ["<Leader>="] = { '"+', desc = "Use the selection register" },
+        ["<Leader>-"] = { '"_', desc = "Use the black hole register" },
+        ["<Leader>a"] = { desc = "AI" },
       },
     },
   },
