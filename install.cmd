1e2bbb37cc9651d8963c6889268c9814ecb19960
Le Tan <tamlokveer@gmail.com>
2026-07-21 23:47:12 +0800

    Remove install.cmd and always copy in install.ps1

--- install.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%1"=="copy" (
-    powershell.exe -ExecutionPolicy Bypass -File ".\install.ps1" -InstallMode copy
-) else (
-    powershell.exe -ExecutionPolicy Bypass -File ".\install.ps1"
-)
--- install.ps1
+++ install.ps1
@@ -2,10 +2,6 @@
 # Le Tan (tamlokveer at gmail.com)
 # https://github.com/tamlok/tanvim
 
-param(
-    [string]$InstallMode
-)
-
 # We use scoop to manage neovim binary.
 function Setup-Neovim
 {
@@ -15,13 +11,8 @@ function Setup-Neovim
         Remove-Item -Path "$filesFolder" -Recurse -Force
     }
 
-    if ($InstallMode -eq "copy") {
-        Write-Host "Copying files to $filesFolder"
-        robocopy ".\" "$filesFolder" /E /MT /XD .git /XF .git* > $null
-    } else {
-        Write-Host "Creating symlink to $filesFolder"
-        New-Item -ItemType Junction -Path $filesFolder -Value $PSScriptRoot
-    }
+    Write-Host "Copying files to $filesFolder"
+    robocopy ".\" "$filesFolder" /E /MT /XD .git /XF .git* > $null
 }
 
 Setup-Neovim
